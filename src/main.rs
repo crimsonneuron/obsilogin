8ecdcf2209df514519d85bcfe21624f0f50585a0
crimsonneuron <james.r.devereux@gmail.com>
2025-10-28 17:16:49 -0400

    added a path dne screen that seems to work sorta

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 use cursive::views::{Dialog, TextView};
+use cursive::Cursive;
 use std::path::Path;
+use std::process::Command;
 use std::env;
 fn main() {
     let mut siv = cursive::default();
-
-     
-
-
+    if !vault_file_path_exists() {
+        path_dne_screen(&mut siv);
+    }
     siv.run()
 }
 
-fn vault_file_path_exists()-> boolean {
+fn vault_file_path_exists()-> bool {
     let args:Vec<String> = env::args().collect();
-    let file_path = Path::new(&args[2]);
+    let file_path = Path::new(&args[1]);
     file_path.exists()
 }
 
-fn path_does_not_exist_screen(&mut cursive_root:CursiveRunnable) {
+fn path_dne_screen(cursive_root: &mut Cursive) {
+    cursive_root.pop_layer();
     cursive_root.add_layer(
         Dialog::around(
             TextView::new("Vault Path Not Found. \n Open obsidian blank?"))
             .title("ObsiLogin: Path Not Found")
-            .button("Open blank", |s| ))
+            .button("Open blank", |_| open_obsidian()));
+}
+
+fn open_obsidian() {
+    Command::new("obsidian");
 }
