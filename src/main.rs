13ad263d727f12389bd32ea5eb8a32bfdc5f1bc4
crimsonneuron <james.r.devereux@gmail.com>
2025-10-29 17:19:51 -0400

    added another screen, for when obsidian can just be launched directly

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,58 @@ use std::process::Command;
 use std::env;
 fn main() {
     let mut siv = cursive::default();
-    if !vault_file_path_exists() {
-        path_dne_screen(&mut siv);
+    let args: Vec<String> = env::args().collect();
+    
+    match usb_status(&args) {
+        USBstatus::NotMounted => usb_dne_screen(&mut siv),
+        USBstatus::NotDecrypted => todo!(),
+        USBstatus::Decrypted => usb_decrypted_screen(&mut siv),
     }
-    siv.run()
+
+    siv.run();
+    siv.quit();
 }
 
-fn vault_file_path_exists()-> bool {
-    let args:Vec<String> = env::args().collect();
-    let file_path = Path::new(&args[1]);
-    file_path.exists()
+enum USBstatus {
+    NotMounted,
+    NotDecrypted,
+    Decrypted,
 }
 
-fn path_dne_screen(cursive_root: &mut Cursive) {
+fn usb_status(args: &Vec<String>) -> USBstatus {
+    let usb_mounted_path = Path::new(&args[1]);
+    let vault_path = Path::new(&args[2]);
+
+    if !usb_mounted_path.exists() {
+        return USBstatus::NotMounted;
+    }
+    else if !vault_path.exists() {
+        return USBstatus::NotDecrypted;
+    }
+    else {
+        return USBstatus::Decrypted;
+    }
+}
+fn usb_dne_screen(cursive_root: &mut Cursive) {
     cursive_root.pop_layer();
     cursive_root.add_layer(
         Dialog::around(
-            TextView::new("Vault Path Not Found. \n Open obsidian blank?"))
-            .title("ObsiLogin: Path Not Found")
+            TextView::new("USB Path Not Found. \n Open obsidian blank?"))
+            .title("ObsiLogin: USB Not Found")
             .button("Open blank", |_| open_obsidian())
             .button("Quit", |s| s.quit()));
+}
+
+fn usb_decrypted_screen(cursive_root: &mut Cursive) {
+    cursive_root.pop_layer();
+    cursive_root.add_layer(
+        Dialog::info("Vault already decrypted, launching obsidian"
+        ).title("ObsiLogin: Launching")
+    );
+    open_obsidian();
 }
  
 fn open_obsidian() {
     Command::new("obsidian").spawn().expect("failed to launch obsidian");
+
 }
